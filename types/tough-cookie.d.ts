1a92b21f2fcc0dbc58437c4f4d8c36c258d47893
raevillena <rhaey1995@gmail.com>
2025-11-12 15:07:23 +0800

    first deploy+fix themes+cockie3

--- types/tough-cookie.d.ts
+++ types/tough-cookie.d.ts
@@ -1,6 +1,8 @@
 declare module "tough-cookie" {
   export class CookieJar {
     constructor(store?: any, options?: any);
+    setCookieSync(cookieOrString: string, url: string, options?: any): void;
+    getCookieStringSync(url: string, options?: any): string;
   }
 }
 
